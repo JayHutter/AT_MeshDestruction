d820b91f2a05588f1742bf6d9388f1e6aba39846
Jay Hutter <jayhutter64@gmail.com>
2021-04-28 01:30:24 +0100

    Game Loop

--- Assets/EscapePod.cs
+++ Assets/EscapePod.cs
@@ -9,8 +9,14 @@ public class EscapePod : MonoBehaviour
     public Light doorLight;
     public GameObject circleLock;
     public float speed = 1.5f;
+    private Transform closePoint;
 
     bool locked = true;
+
+    bool opening = false;
+    bool closing = false;
+
+    public GameObject canvas;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +25,9 @@ public class EscapePod : MonoBehaviour
         {
             Destroy(this);
         }
+
+        closePoint = transform;
+        canvas.SetActive(false);
     }
 
     public void Unlock()
@@ -43,14 +52,44 @@ public class EscapePod : MonoBehaviour
 
         if (!locked && playerDistance.magnitude <= 5)
         {
+            opening = true;
+           
+        }
+
+        if (opening)
+        {
             Vector3 pos = Vector3.Lerp(door.transform.position, endPoint.position, Time.deltaTime * speed);
             door.transform.position = pos;
 
             Vector3 distance = door.transform.position - endPoint.position;
             if (distance.magnitude <= 0.1f)
             {
+                opening = false;
+                locked = true;
+            }
+        }
+
+        if (closing)
+        {
+            Vector3 pos = Vector3.Lerp(door.transform.position, closePoint.position, Time.deltaTime * speed);
+            door.transform.position = pos;
+
+            Vector3 distance = door.transform.position - closePoint.position;
+            if (distance.magnitude <= 0.15f)
+            {
                 Destroy(this);
             }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("S");
+            closing = true;
+            canvas.SetActive(true);
         }
     }
 }
--- Assets/Scripts/SlicedObject.cs
+++ Assets/Scripts/SlicedObject.cs
@@ -4,48 +4,4 @@ using UnityEngine;
 
 public class SlicedObject : MonoBehaviour
 {
-    bool checkSize = true;
-
-    private void Update()
-    {
-        if (checkSize)
-        {
-            checkSize = false;
-            float volume = Volume() * 100;
-        }
-    }
-
-
-    float VolumeOfTri(Vector3 pA, Vector3 pB, Vector3 pC)
-    {
-        float scale = transform.localScale.x * transform.localScale.y * transform.localScale.z;
-
-        float vCBA = pC.x * pB.y * pA.z;
-        float vBCA = pB.x * pC.y * pA.z;
-        float vCAB = pC.x * pA.y * pB.z;
-        float vACB = pA.x * pC.y * pB.z;
-        float vBAC = pB.x * pA.y * pC.z;
-        float vABC = pA.x * pB.y * pC.z;
-
-        return (1.0f / 6.0f) * (-vCBA + vBCA + vCAB - vACB - vBAC + vABC) * scale;
-    }
-
-    float Volume()
-    {
-        float vol = 0.0f;
-        Mesh mesh = GetComponent<MeshFilter>().mesh;
-        Vector3[] verts = mesh.vertices;
-        int[] tris = mesh.triangles;
-
-        for (int i = 0; i < tris.Length; i += 3)
-        {
-            Vector3 pointA = verts[tris[i]];
-            Vector3 pointB = verts[tris[i + 1]];
-            Vector3 pointC = verts[tris[i + 2]];
-
-            vol += VolumeOfTri(pointA, pointB, pointC);
-        }
-
-        return Mathf.Abs(vol);
-    }
 }
